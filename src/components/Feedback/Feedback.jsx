944170a54f59cabd822ccbbb3ad482e7cbd34023
Denis Burdin <107995658+d1on1s1y@users.noreply.github.com>
2023-03-12 11:18:42 +0200

    Initial commit

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,42 @@
+import Statistic from 'components/Statistic';
+import { Component } from 'react';
+import s from './Feedback.module.css';
+
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleOnClick = e => {
+    return this.setState(prevState => ({
+      ...prevState,
+      [e.target.name]: prevState[e.target.name] + 1,
+    }));
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <>
+        <h3 className={s.title}>Please leave feedback</h3>
+        <div onClick={this.handleOnClick} className={s.btnTeam}>
+          <button type="button" name="good" className={s.btn}>
+            Good
+          </button>
+          <button type="button" name="neutral" className={s.btn}>
+            Neutral
+          </button>
+          <button type="button" name="bad" className={s.btn}>
+            Bad
+          </button>
+        </div>
+
+        <Statistic state={this.state} />
+      </>
+    );
+  }
+}
+
+export default Feedback;
